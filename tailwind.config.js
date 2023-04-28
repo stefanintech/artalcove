d4b5fbeef35de17f6edb348c27bba73b9c7caed2
Stefan <stefantaitano@gmail.com>
2023-04-27 22:42:46 -0600

    create header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#87805E',
+      }
+    },
   },
   plugins: [],
 }
