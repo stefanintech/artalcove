fa0da624a9316a99fabf50eb87168ad97abc8515
Stefan <stefantaitano@gmail.com>
2023-04-30 07:09:47 -0600

    create route for login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
 import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <Routes>
       <Route index element={<IndexPage />} />
+      <Route path='/login' element={<LoginPage />} />
     </Routes>
       
   )
