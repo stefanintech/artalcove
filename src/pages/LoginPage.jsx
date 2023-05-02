a6bf37652f0afa1940297ae818393ead8d788267
Stefan <stefantaitano@gmail.com>
2023-05-02 16:42:55 -0600

    edit login text for register account

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -10,7 +10,7 @@ export default function LoginPage() {
                     <input type="password" placeholder='your password here' />
                 <button className="primary">Login</button>
                 <div className="text-center py-2 text-gray-500">
-                    Don't have an account yet? <Link className="underline text-black" to={'/register'}>Register Here</Link>
+                    No account yet? <Link className="underline text-black" to={'/register'}>Register Here</Link>
                 </div>
                 </form>
             </div>
