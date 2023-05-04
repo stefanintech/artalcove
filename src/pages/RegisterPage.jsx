3910d1dd2c589d0f75f9982389caeee0b1ee9923
Stefan <stefantaitano@gmail.com>
2023-05-03 23:05:31 -0600

    connect registration page to database

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function RegisterPage() {
+    const [name,setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    async function registerUser(ev) {
+        ev.preventDefault()
+        try {
+            await axios.post('/register', {
+                name,
+                email,
+                password,
+            });
+            alert('Registration successful. Now you can log!')
+        } catch (error) {
+            alert('Registration failed. Please try again later')
+        }
+    }
+    return (
+        <div className="mt-4 grow flex items-center justify-around">
+            <div className="mb-64">
+                <h1 className="text-4xl text-center mb-4">Register</h1>
+                <form className="max-w-md mx-auto" onSubmit={registerUser}>
+                    <input type="text" 
+                    placeholder="John Doe" 
+                    value={name} 
+                    onChange={ev => setName(ev.target.value)} />
+                    <input type="email" 
+                    placeholder='your@email.com' 
+                    value={email}
+                    onChange={ev => setEmail(ev.target.value)} />
+                    <input type="password" 
+                    placeholder='your password here'
+                    value={password}
+                    onChange={ev => setPassword(ev.target.value)} />
+                <button className="primary">Register</button>
+                <div className="text-center py-2 text-gray-500">
+                    Already a member? <Link className="underline text-black" to={'/login'}>Login Here</Link>
+                </div>
+                </form>
+            </div>
+        </div>
+    )
+}
