4d0a5e8303ba24f96fdc4030ae05f33f039de4d2
Stefan <stefantaitano@gmail.com>
2023-05-03 23:35:27 -0600

    add new packages

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Layout from './Layout';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
