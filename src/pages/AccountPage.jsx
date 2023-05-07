a928f099cc13e0b8fe4d4152b619848f8d432ebf
Stefan <stefantaitano@gmail.com>
2023-05-06 22:59:03 -0600

    add ready status for logged in user

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,7 +1,16 @@
+import { useContext } from "react"
+import { UserContext } from "../UserContext"
+import { Navigate } from "react-router-dom"
+
 export default function AccountPage() {
+    const {ready, user} = useContext(UserContext)
+
+    if (ready && !user) {
+        return <Navigate to={'/login'} />
+    }
     return (
         <div>
-            account page
+            account page for {user.name}
         </div>
     )
 }
