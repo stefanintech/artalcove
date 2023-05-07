8a09dfc47a372cf9c689600e99beb6f675407bc5
Stefan <stefantaitano@gmail.com>
2023-05-06 23:44:58 -0600

    add styling for logged in user and logout button.

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,9 +1,18 @@
 import { useContext } from "react"
 import { UserContext } from "../UserContext"
 import { Link, Navigate, useParams } from "react-router-dom"
+import axios from "axios";
 
 export default function AccountPage() {
     const {ready, user} = useContext(UserContext)
+    let {subpage} = useParams();
+    if (subpage === undefined) {
+        subpage = 'profile'
+    }
+
+    async function logout() {
+        await axios.post('/logout')
+    }
 
     if (!ready) {
         return 'Loading...'
@@ -13,11 +22,11 @@ export default function AccountPage() {
         return <Navigate to={'/login'} />
     }
 
-    const {subpage} = useParams();
+
 
     function linkClasses (type=null) {
         let classes = 'py-2 px-6';
-        if (type === subpage || (subpage === undefined && type === 'profile')) {
+        if (type === subpage) {
             classes += ' bg-primary text-white rounded-full'
         }
         return classes
@@ -26,11 +35,17 @@ export default function AccountPage() {
     
     return (
         <div>
-            <nav className="w-full flex justify-center mt-8 gap-2">
+            <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
                 <Link className={linkClasses('profile')} to={'/account'}>My profile</Link>
                 <Link className={linkClasses('bookings')} to={'/account/bookings'}>My bookings</Link>
                 <Link className={linkClasses('places')} to={'/account/places'}>My accommodations</Link>
             </nav>
+            {subpage === 'profile' && (
+                <div className='text-center max-w-lg mx-auto'>
+                    Logged in as {user.name} ({user.email})<br />
+                    <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
+                </div>
+            )}
         </div>
     )
 }
