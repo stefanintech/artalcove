9f40709112c86d0646239608d5df811058d4fffa
Stefan <stefantaitano@gmail.com>
2023-05-06 23:09:37 -0600

    add loading feature if user is not ready

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -5,9 +5,16 @@ import { Navigate } from "react-router-dom"
 export default function AccountPage() {
     const {ready, user} = useContext(UserContext)
 
+    if (!ready) {
+        return 'Loading...'
+    }
+
     if (ready && !user) {
         return <Navigate to={'/login'} />
     }
+
+
+    
     return (
         <div>
             account page for {user.name}
