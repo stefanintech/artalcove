355cd97718fde6924e418a78552f666362ea26ab
Stefan <stefantaitano@gmail.com>
2023-05-10 22:21:26 -0600

    add form and photos button

--- src/pages/PlacesPage.jsx
+++ src/pages/PlacesPage.jsx
@@ -1,16 +1,38 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function PlacesPage() {
+    const {action} = useParams();
     return (
         <div>
-            <div className="text-center">
-                <Link className='inline-flex gap-1 bg-primary text-white py2 px-6 rounded-full' to={'/account/places/new'}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"     
-                className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                </svg>
-                Add new Place</Link>
-            </div>
+            {action !== 'new' && (
+                <div className="text-center">
+                    <Link className='inline-flex gap-1 bg-primary text-white py2 px-6 rounded-full' to={'/account/places/new'}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"     
+                        className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+                        Add new Place
+                    </Link>
+                </div>
+            )}
+            {action === 'new' && (
+                <div>
+                    <form>
+                        <h2 className="text-2xl mt-4">Title</h2>
+                        <p className="text-gray-500 text-sm">Title to catch the creator's eye</p>
+                        <input type="text" placeholder="title, for exmaple: My lovely apartment" />
+                        <h2 className="text-2xl mt-4">Address</h2>
+                        <p className="text-gray-500 text-sm">Address to your space</p>
+                        <input type="text" placeholder="address" />
+                        <h2 className="text-2xl mt-4">Photos</h2>
+                        <p className="text-gray-500 text-sm">Showcase your best shots</p>
+                        <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+                            <button className="border bg-transparent rounded--2xl p-8 text-2xl text-gray-600">+</button>
+
+                        </div>
+                    </form>
+                </div>
+            )}
         </div>
     )
 }
