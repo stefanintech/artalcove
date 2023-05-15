a75e34f06e8ad125fffe04377a35d60b517778ae
Stefan <stefantaitano@gmail.com>
2023-05-15 08:01:29 -0600

    refactoring PlagesPage code. Added perks component

--- src/pages/PlacesPage.jsx
+++ src/pages/PlacesPage.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import Perks from "../Perks";
 
 export default function PlacesPage() {
     const {action} = useParams();
+    const [title,setTitle] = useState('');
+    const [address,setAddress] = useState('');
+    const [addedPhotos, setAddedPhotos] = useState('');
+    const [photoLink, setPhotoLink] = useState('');
+    const [description, setDescription] = useState('');
+    const [perks,setPerks] = useState([]);
+    const [extraInfo,setExtraInfo] = useState('');
+    const [checkIn,setCheckIn] = useState('');
+    const [checkOut,setCheckOut] = useState('');
+    const [maxGuests,setMaxGuests] = useState(1);
+    function inputHeader(text) {
+        return (
+            <h2 className="text-2xl mt-4">{text}</h2>
+        )
+    }
+    function inputDescription(text) {
+        <p className="text-gray-500 text-sm">{text}</p>
+    }
+    function preInput(header, description) {
+        return (
+            <div>
+                {inputHeader(header)}
+                {inputDescription(description)}
+            </div>
+        )
+    }
     return (
         <div>
             {action !== 'new' && (
@@ -18,14 +46,11 @@ export default function PlacesPage() {
             {action === 'new' && (
                 <div>
                     <form>
-                        <h2 className="text-2xl mt-4">Title</h2>
-                        <p className="text-gray-500 text-sm">Title to catch the creator eye</p>
-                        <input type="text" placeholder="title, for exmaple: My lovely apartment" />
-                        <h2 className="text-2xl mt-4">Address</h2>
-                        <p className="text-gray-500 text-sm">Address to your space</p>
+                        {preInput('Title', 'Title for your place. Should be short and sweet.')}
+                        <input type="text" placeholder="title, for example: My lovely apartment" />
+                        {preInput('Address', 'Address to this place')}
                         <input type="text" placeholder="address" />
-                        <h2 className="text-2xl mt-4">Photos</h2>
-                        <p className="text-gray-500 text-sm">Showcase your best shots</p>
+                        {preInput('Photos', 'Showcase your best shots')}
                         <div className="flex gap-2">
                             <input type="text" placeholder={'Add using a link ...jpg'} />
                             <button className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
@@ -40,76 +65,13 @@ export default function PlacesPage() {
                             Upload
                             </button>
                         </div>
-                        <h2 className="text-2xl mt-4">Description</h2>
-                        <p className="text-gray-500 text-sm">Talk mad about your place</p>
+                        {preInput('Description', 'Talk mad about your place')}
                         <textarea />
-                        <h2 className="text-2xl mt-4">Perks</h2>
-                        <p className="text-gray-500 text-sm">Select perks for your space</p>
-                        <div className="grid mt-2 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
-                            <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
-                                <input type="checkbox" />
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} 
-                                stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106     
-                                    11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 
-                                    18.22l-.53.53-.53-.53a.75.75 0 011.06 0z" />
-                                </svg>
-                                <span>Wifi</span>
-                            </label>
-                            <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
-                                <input type="checkbox" />
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} 
-                                stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" 
-                                    />
-                                </svg>
-                                <span>Restrooms</span>
-                            </label>
-                            <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
-                                <input type="checkbox" />
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} 
-                                stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 8.25v-1.5m0 1.5c-1.355 0-2.697.056-4.024.166C6.845 8.51 6 9.473 6 10.608v2.513m6-4.87c1.355 0 2.697.055 4.024.165C17.155 8.51 18 9.473 18 10.608v2.513m-3-4.87v-1.5m-6 1.5v-1.5m12 9.75l-1.5.75a3.354 3.354 0 01-3 0 3.354 3.354 0 00-3 0 3.354 3.354 0 01-3 0 3.354 3.354 0 00-3 0 3.354 3.354 0 01-3 0L3 16.5m15-3.38a48.474 48.474 0 00-6-.37c-2.032 0-4.034.125-6 .37m12 0c.39.049.777.102 1.163.16 1.07.16 1.837 1.094 1.837 2.175v5.17c0 .62-.504 1.124-1.125 1.124H4.125A1.125 1.125 0 013 20.625v-5.17c0-1.08.768-2.014 1.837-2.174A47.78 47.78 0 016 13.12M12.265 3.11a.375.375 0 11-.53 0L12 2.845l.265.265zm-3 0a.375.375 0 11-.53 0L9 2.845l.265.265zm6 0a.375.375 0 11-.53 0L15 2.845l.265.265z" />
-                                </svg>
-                                <span>Refreshment Area</span>
-                            </label>
-                            <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
-                                <input type="checkbox" />
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} 
-                                stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 
-                                    0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 
-                                    0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 
-                                    18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 
-                                    00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
-                                </svg>
-                                <span>Free Parking</span>
-                            </label>
-                            <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
-                                <input type="checkbox" />
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} 
-                                stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 
-                                    2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-
-                                    1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 
-                                    1.125z" />
-                                </svg>
-                                <span>Storage Space</span>
-                            </label>
-                            <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
-                                <input type="checkbox" />
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
-                                </svg>
-                                <span>Natural Lighting</span>
-                            </label>
-                        </div>
-                        <h2 className="text-2xl mt-4">Extra info</h2>
-                        <p className="text-gray-500 text-sm">House rules, etc.</p>
+                        {preInput('Perks', 'Select perks for your space')}
+                        <Perks />
+                        {preInput('Extra info', 'House rules, etc')}
                         <textarea />
-                        <h2 className="text-2xl mt-4">Check in&out times</h2>
-                        <p className="text-gray-500 text-sm">add check in and out times, remember to have time window for cleaning the room 
-                        between guests</p>
+                        {preInput('Check in&out times', 'Add times')}
                         <div className="grid gap-2 sm:grid-cols-3">
                             <div>
                                 <h3 className="mt-2 -mb-1">Check in time</h3>
@@ -124,9 +86,7 @@ export default function PlacesPage() {
                                 <input type="text" />
                             </div>
                         </div>
-                        <div>
-                            <button className="primary my-4">Save</button>
-                        </div>
+                        <button className="primary my-4">Save</button>
                     </form>
                 </div>
             )}
